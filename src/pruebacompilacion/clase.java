0c9b9c8d3a5f3b5623769f4b30bc117a31000dcf
S_Uboldi <solmaruboldi@gmail.com>
2023-03-14 17:36:14 -0300

    Primer commit ok, configuración lista para comenzar a trabajar

--- src/pruebacompilacion/clase.java
+++ src/pruebacompilacion/clase.java
@@ -4,6 +4,7 @@ public class clase {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("Ahora sí está todo configurado. Creo que se puede empezar a trabajar.");
 	}
 
 }
