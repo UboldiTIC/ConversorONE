a3d9bd348a3644375e93571f87e06e351d9a8d6a
Tuta <solmaruboldi@gmail.com>
2023-03-24 15:25:25 -0300

    Avance en interfaz gráfica - botones y panel secundario

--- src/MenuPrincipalConversor/Contenedor.java
+++ src/MenuPrincipalConversor/Contenedor.java
@@ -1,17 +1,24 @@
 package MenuPrincipalConversor;
 
 import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
 
 public class Contenedor extends JFrame {
 
 	public JPanel panelPrincipal;
 
+	//Área de trabajo principal:
 	public Contenedor() {
-		setSize(800, 640);
+		setSize(800, 600);
 		setLocationRelativeTo(null);
 		setTitle("Conversor ONE (Alura + Oracle)");
 		//this.getContentPane().setBackground(Color.DARK_GRAY);
@@ -21,16 +28,88 @@ public class Contenedor extends JFrame {
 
 	private void iniciarOpciones() {
 		iniciarPanel();
-		seleccionarMonedaUno();
-		seleccionarMonedaDos();
+		tituloPrincipal();
+		crearBotones();
+		
+		//seleccionarMonedaUno();
+		//seleccionarMonedaDos();
 	}
+	
+	//Panel principal:
 
 	private void iniciarPanel() {
 		panelPrincipal = new JPanel();
 		panelPrincipal.setBackground(Color.DARK_GRAY);
 		panelPrincipal.setLayout(null);
 		this.getContentPane().add(panelPrincipal);
+		
+		
+		iniciarPanelTemperatura(); // revisar panel 2
 	}
+	
+	//Título Panel:
+	
+	private void tituloPrincipal() {
+		JLabel titulo = new JLabel("Conversor ONE",SwingConstants.CENTER);
+		//titulo.setLayout(null);
+		titulo.setBounds(150, 5, 500, 80);
+		titulo.setForeground(Color.WHITE);
+		//etiqueta.setOpaque(true); //establecemos pintar el fondo de la etiqueta.
+		titulo.setBackground(Color.BLACK);
+		titulo.setFont(new Font("cooper black",Font.PLAIN,40));
+		panelPrincipal.add(titulo);
+	}
+	
+	
+	//Botones selectores principales:
+	
+	private void crearBotones() {
+		//Botón seleccionar temperatura:
+		JButton botonTemperatura = new JButton("Temperatura");
+		botonTemperatura.setLayout(null);
+		botonTemperatura.setBounds(150, 80, 180, 40);
+		botonTemperatura.setForeground(Color.BLACK);
+		botonTemperatura.setFont(new Font("arial",Font.BOLD,20));
+		panelPrincipal.add(botonTemperatura);
+		//Botón seleccionar monedas:
+		JButton botonMonedas = new JButton("Monedas");
+		botonMonedas.setLayout(null);
+		botonMonedas.setBounds(450, 80, 180, 40);
+		botonMonedas.setForeground(Color.BLACK);
+		botonMonedas.setFont(new Font("arial",Font.BOLD,20));
+		panelPrincipal.add(botonMonedas);
+		
+	}
+	//Panel conversor de temperaturas:
+	private void iniciarPanelTemperatura() {
+		JPanel panelTemperatura = new JPanel();
+		panelTemperatura.setBounds(50, 150, 700, 400);
+		panelTemperatura.setLayout(null);
+		panelTemperatura.setBackground(Color.LIGHT_GRAY);
+		
+		panelPrincipal.add(panelTemperatura); //revisar panel 2.
+		
+		//Revisar radiobotones:
+		JRadioButton botonFahrenheit = new JRadioButton("Fahrenheit a Celsius", true);
+		botonFahrenheit.setBounds(150,10,200,100);
+		botonFahrenheit.setBackground(Color.LIGHT_GRAY);
+		panelTemperatura.add(botonFahrenheit);
+		JRadioButton botonCelsius = new JRadioButton("Celsius a Fahrenheit", true);
+		botonCelsius.setBounds(450,10,200,100);
+		botonCelsius.setBackground(Color.LIGHT_GRAY);
+		panelTemperatura.add(botonCelsius);
+		ButtonGroup grupoRadioBotones = new ButtonGroup();
+		grupoRadioBotones.add(botonFahrenheit);
+		grupoRadioBotones.add(botonCelsius);
+		
+		//Ingresar valor:
+		
+		//Agregar boton convertir y crear boton convertir y reiniciar en panel principal
+
+	}
+	
+	
+	//Panel Conversor de monedas:
 
 	private void seleccionarMonedaUno() {
 		String[] monedas = {"Dólar", "Euros", "Libras Esterlinas", "Yen Japonés", "Won Sul-Coreano" };
@@ -47,5 +126,7 @@ public class Contenedor extends JFrame {
 		listaMonedas.setBounds(400, 20, 200, 30);
 		panelPrincipal.add(listaMonedas);
 	}
+	
+	
 	
 }
