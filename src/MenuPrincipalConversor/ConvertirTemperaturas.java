1629bfecb8237ddd264168c60bc1d7221342b290
Tuta <solmaruboldi@gmail.com>
2023-04-02 23:25:01 -0300

    Completados los métodos para convertir temperaturas

--- src/MenuPrincipalConversor/ConvertirTemperaturas.java
+++ src/MenuPrincipalConversor/ConvertirTemperaturas.java
@@ -2,34 +2,42 @@ package MenuPrincipalConversor;
 
 public class ConvertirTemperaturas {
 
-	private double celsius;
-	double resultado;
-	public double fahrenheit = resultado;
-	//private double resultado = this.fahrenheit;
+	// Convertir de grados celsius a Fahrenheit
+	public double convertirCelsiusAFahrenheit(double valorIngresado) {
+		double fahrenheit = (valorIngresado * 1.8) + 32;
+		return fahrenheit;
+	}
+	// Convertir de grados Fahrenheit a Celsius
+	public static double convertirFahrenheitACelsius(double valorIngresado) {
+		double celsius = (valorIngresado - 32) / 1.8;
+		return celsius;
+	}
+	// Convertir de grados kelvin a Celsius
+	public static double convertirKelvinACelsius(double valorIngresado) {
+		double celsius = valorIngresado - 273.15;
+		return celsius;
+	}
+	// Convertir de grados Celsius a Kelvin
+	public static double convertirCelsiusAKelvin(double valorIngresado) {
+		double kelvin = valorIngresado + 273.15;
+		return kelvin;
+	}
+	// Convertir de Fahrenheit a Kelvin
+	public static double convertirFahrenheitAKelvin(double valorIngresado) {
+		double kelvin = (valorIngresado - 32) / 1.8 + 273.15;
+		return kelvin;
+	}
+	// Convertir de Kelvin a Fahrenheit
+		public static double convertirKelvinAFarenheit(double valorIngresado) {
+			double fahrenheit = (valorIngresado - 273.15) * 1.8 + 32;
+			return fahrenheit;
+		}
 	
-//	public double AsignarCelsius(double celsius) {
-//		return this.celsius = this.celsius + celsius;
+		
+////	// testear
+////	public static void main(String[] args) {
+////		double valorIngresado = 25; // ejemplo
+////		double fahrenheit = convertirKelvinAFarenheit(valorIngresado);
+////		System.out.println(valorIngresado + " grados, son " + fahrenheit + " grados.");
 //	}
-
-	public double convertirCelsius() {
-		resultado = (celsius*1.8)+32;
-		return resultado;
-	}
-	
-	public double getCelsius() {
-		return this.celsius;
-	}
-
-	public void setCelsius(double celsius) {
-		this.celsius = celsius;
-	}
-
-	public double getFahrenheit() {
-		return this.fahrenheit;
-	}
-
-	public void setFahrenheit(double fahrenheit) {
-		this.fahrenheit = fahrenheit;
-	}
-	
 }
